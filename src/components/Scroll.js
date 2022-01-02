ccf12be9f9f608b2a603b82677907a2be31ed937
Yin <matt3982@ip148-124.wlan.ntust.edu.tw>
2022-01-02 18:22:08 +0800

    updating scroll.js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,6 +4,7 @@ const Scroll = (props) => {
     return(
         <div style={{overflowY: 'scroll', border: '0.1px black solid', height: '620px' }}>
             {props.children}  
+            {/*prop.children包含了所有被<Scroll>包裹的子tag*/}
         </div>
     )
 }
